b585d7fd69545e3ebe49a811d2558b92a01fd802
fArtuCh <arturchrzan@gmail.com>
2026-04-20 12:40:16 +0200

    d

--- MyTestApp.ApiService/obj/Debug/net10.0/MyTestApp.ApiService.AssemblyInfo.cs
+++ MyTestApp.ApiService/obj/Debug/net10.0/MyTestApp.ApiService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTestApp.ApiService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52db846fcab1b87d040b9ff58319b79dbd3371d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0710ad777612705480518961bf26ffb56740d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTestApp.ApiService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTestApp.ApiService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MyTestApp.AppHost/obj/Debug/net10.0/MyTestApp.AppHost.AssemblyInfo.cs
+++ MyTestApp.AppHost/obj/Debug/net10.0/MyTestApp.AppHost.AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTestApp.AppHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52db846fcab1b87d040b9ff58319b79dbd3371d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0710ad777612705480518961bf26ffb56740d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTestApp.AppHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTestApp.AppHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
